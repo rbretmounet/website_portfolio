c2dc1f4061b25d1e5f607a3f8c96021500c97da9
rbretmounet <rbretmounet@gmail.com>
2020-12-05 19:20:16 -0800

    removed unused files and added class to nav bar.

--- JavaScript/scrollingScript.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $(".menu a").on('click', function(){
-        $("html, body").animate({
-            scrollTop: $($.attr(this,'href')).offset().top
-        }, 500);
-    });
-});
