416de2581b2b235bf69f556cd7ce3429dd13d9df
rbretmounet <rbretmounet@gmail.com>
2020-12-05 17:33:44 -0800

    Added nav bar that dissapears on the home page.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,5 @@
 new fullpage('#fullpage', {
   licenseKey: 'YOUR KEY HERE',
-  anchors: ['','about','projects','contact'],
-  sectionsColor: [" ", "#f8f8ff","#252934"]
+  anchors: ['home','about','projects','contact'],
+  sectionsColor: [" ", "#f8f8ff","#252934"],
 });
