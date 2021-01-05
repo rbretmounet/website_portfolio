c74c5795a304dab1434f1e3a09fc65db03d60a6f
rbretmounet <rbretmounet@gmail.com>
2021-01-04 21:43:47 -0800

    Added modals and added project windows.

---
+++ JavaScript/modal.js
@@ -0,0 +1,26 @@
+
+function modal_handle(modal){
+  console.log(modal)
+  // Get the modal
+  var modal = document.getElementById(modal);
+  console.log(modal)
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  modal.style.display = "block";
+  
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
