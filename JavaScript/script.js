b3ef32be7aeac7886c09898f78d6cd8cee922106
rbretmounet <rbretmounet@gmail.com>
2021-01-22 13:52:08 -0800

    Added mobile view

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,7 @@
 new fullpage('#fullpage', {
   anchors: ['home','about','projects','contact'],
   sectionsColor: [" ", "#EBEAEA","#F8F8FF","#333232"],
+  paddingTop: '0px',
+  paddingBottom: '100px',
   scrollOverflow: true
 });
